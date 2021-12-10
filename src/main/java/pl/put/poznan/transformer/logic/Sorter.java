dfbf0f815acbc6208cde6a3c9b2f2e8e8ca6cb53
= <=>
2021-12-10 16:14:15 +0100

    Adding max_it and direction to Sorter

--- src/main/java/pl/put/poznan/transformer/logic/Sorter.java
+++ src/main/java/pl/put/poznan/transformer/logic/Sorter.java
@@ -3,5 +3,5 @@ package pl.put.poznan.transformer.logic;
 import java.util.List;
 
 public interface Sorter {
-    <E extends Comparable<E>> void sort(List<E> unsorted_list);
+    <E extends Comparable<E>> void sort(List<E> unsorted_list, int max_it, boolean ascending);
 }
