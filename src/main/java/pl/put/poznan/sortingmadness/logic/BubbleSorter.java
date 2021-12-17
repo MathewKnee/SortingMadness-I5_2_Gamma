f32c35e8cd31071d09b961a8cb37e5c8e67e0bb5
= <=>
2021-12-17 21:33:43 +0100

    Adding Object sorting to BubbleSorter and CocktailSorter

--- src/main/java/pl/put/poznan/sortingmadness/logic/BubbleSorter.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/BubbleSorter.java
@@ -1,5 +1,7 @@
 package pl.put.poznan.sortingmadness.logic;
 
+import org.json.JSONObject;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -17,13 +19,27 @@ public class BubbleSorter implements Sorter {
                         if (direction_switch * ((String) unsorted_list.get(j - 1)).compareToIgnoreCase((String) unsorted_list.get(j)) > 0) {
                             Collections.swap(unsorted_list, j - 1, j);
 
-                        } else {
-
-
-                            if (direction_switch * unsorted_list.get(j - 1).compareTo(unsorted_list.get(j)) > 0) {
-                                Collections.swap(unsorted_list, j - 1, j);
-                            }
                         }
+                    } else {
+                        if (direction_switch * unsorted_list.get(j - 1).compareTo(unsorted_list.get(j)) > 0) {
+                                Collections.swap(unsorted_list, j - 1, j);
+                        }
+                    }
+                }
+            } else {
+                break;
+            }
+
+        }
+    }
+    public  void sort(List<JSONObject> unsorted_list, int max_it, boolean ascending, JSONComparator comparator) {
+        int sizeOfList = unsorted_list.size();
+        int direction_switch = ascending ? 1 : -1;
+        for (int i = 0; i < sizeOfList; i++) {
+            if (i < max_it) {
+                for (int j = 0; j < (sizeOfList - i); j++) {
+                    if (direction_switch * comparator.compare(unsorted_list.get(j - 1),unsorted_list.get(j)) > 0) {
+                        Collections.swap(unsorted_list, j - 1, j);
                     }
                 }
             } else {
--- src/main/java/pl/put/poznan/sortingmadness/logic/CocktailSorter.java
+++ src/main/java/pl/put/poznan/sortingmadness/logic/CocktailSorter.java
@@ -5,8 +5,21 @@ import org.json.JSONObject;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Concrete strategy of Sorter. Used to sort lists using cocktail sort algorithm.
+ * @author Mateusz Kolano
+ * @version 1.1
+ */
 public class CocktailSorter implements Sorter{
-
+    /**
+     * Sorts the given list using cocktail sort algorithm.
+     * One iteration means one loop left to right and one loop right to left.
+     *
+     * @param unsorted_list - list of Comparable objects to be sorted
+     * @param max_it - specifies maximum number of iterations for the cocktail sort (if max_it<=0 then inf)
+     * @param ascending - specifies direction of sorting if true then ascending else false
+     * @param <E> - self Comparable object
+     */
     public <E extends Comparable<E>> void sort(List<E> unsorted_list, int max_it, boolean ascending){
         boolean swapped = true;
         int direction_switch = ascending ? 1:-1;
@@ -19,7 +32,9 @@ public class CocktailSorter implements Sorter{
             if(it_count >= it_max) break;
             swapped = false;
             for(int i = start; i < end - 1; ++i){
-                if(direction_switch * unsorted_list.get(i).compareTo(unsorted_list.get(i+1)) >0){
+                if(direction_switch * (unsorted_list.get(i) instanceof String ?
+                        ((String)unsorted_list.get(i)).compareToIgnoreCase((String)unsorted_list.get(i+1)):
+                        unsorted_list.get(i).compareTo(unsorted_list.get(i+1))) > 0){
                     Collections.swap(unsorted_list,i,i+1);
                     swapped = true;
                 }
@@ -27,15 +42,25 @@ public class CocktailSorter implements Sorter{
             if(!swapped) break;
             end = end - 1;
             for (int i = end - 1; i >= start; i--){
-                if(direction_switch * unsorted_list.get(i).compareTo(unsorted_list.get(i+1)) >0){
+                if(direction_switch * (unsorted_list.get(i) instanceof String ?
+                                ((String)unsorted_list.get(i)).compareToIgnoreCase((String)unsorted_list.get(i+1)):
+                                unsorted_list.get(i).compareTo(unsorted_list.get(i+1))) > 0){
                     Collections.swap(unsorted_list,i,i+1);
-                    swapped = true;
                 }
             }
             start += 1;
             it_count += 1;
         }
     }
+    /**
+     * Sorts the given list using cocktail sort algorithm.
+     * One iteration means one loop left to right and one loop right to left.
+     *
+     * @param unsorted_list - list of JSONObjects to be sorted
+     * @param max_it - specifies maximum number of iterations for the cocktail sort (if max_it<=0 then inf)
+     * @param ascending - specifies direction of sorting if true then ascending else false
+     * @param comparator - JSONComparator that compares those two objects based on given keys
+     */
     public void sort(List<JSONObject> unsorted_list, int max_it, boolean ascending, JSONComparator comparator){
         boolean swapped = true;
         int direction_switch = ascending ? 1:-1;
@@ -58,7 +83,7 @@ public class CocktailSorter implements Sorter{
             for (int i = end - 1; i >= start; i--){
                 if(direction_switch * comparator.compare(unsorted_list.get(i),unsorted_list.get(i+1)) >0){
                     Collections.swap(unsorted_list,i,i+1);
-                    swapped = true;
+
                 }
             }
             start += 1;
