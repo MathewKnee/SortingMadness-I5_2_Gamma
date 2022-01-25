c56f960530ddad968ff40126fd04391d668c847d
= <=>
2022-01-25 11:52:33 +0100

    Added automated tests execution

---
+++ src/test/java/pl/put/poznan/sortingmadness/CocktailSorterTest.java
@@ -0,0 +1,158 @@
+package pl.put.poznan.sortingmadness;
+
+import org.junit.jupiter.api.Test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import pl.put.poznan.sortingmadness.logic.CocktailSorter;
+import pl.put.poznan.sortingmadness.logic.JSONComparator;
+
+import java.util.*;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CocktailSorterTest {
+    List<Comparable> mockList;
+    Comparable mockComparable;
+    JSONObject mockJSONObject;
+    List<JSONObject> mockJSONList;
+
+    @Test
+    void testAscendingSimple(){
+        mockList        = mock(List.class);
+        mockComparable  = mock(Comparable.class);
+        when(mockList.size()).thenReturn(4);
+        when(mockList.get(0)).thenReturn(-1);
+        when(mockList.get(1)).thenReturn(100);
+        when(mockList.get(2)).thenReturn(12);
+        when(mockList.get(3)).thenReturn(176);
+
+        CocktailSorter cocktailSorter = new CocktailSorter();
+        cocktailSorter.sort(mockList, 0, true);
+        verify(mockList, times(1)).size();
+        verify(mockList, times(28)).get(any(Integer.class));
+
+        // --------------------------------------
+
+        mockList        = mock(List.class);
+        mockComparable  = mock(Comparable.class);
+        when(mockList.size()).thenReturn(4);
+        when(mockList.get(0)).thenReturn(100);
+        when(mockList.get(1)).thenReturn(100);
+        when(mockList.get(2)).thenReturn(100);
+        when(mockList.get(3)).thenReturn(100);
+
+        cocktailSorter.sort(mockList, 0, true);
+        verify(mockList, times(1)).size();
+        verify(mockList, times(9)).get(any(Integer.class));
+    }
+
+    @Test
+    void testDescendingSimple(){
+        mockList        = mock(List.class);
+        mockComparable  = mock(Comparable.class);
+        when(mockList.size()).thenReturn(4);
+        when(mockList.get(0)).thenReturn(-1);
+        when(mockList.get(1)).thenReturn(100);
+        when(mockList.get(2)).thenReturn(12);
+        when(mockList.get(3)).thenReturn(176);
+
+        CocktailSorter cocktailSorter = new CocktailSorter();
+        cocktailSorter.sort(mockList, 0, false);
+        verify(mockList, times(1)).size();
+        verify(mockList, times(25)).get(any(Integer.class));
+
+        // --------------------------------------
+
+        mockList        = mock(List.class);
+        mockComparable  = mock(Comparable.class);
+        when(mockList.size()).thenReturn(4);
+        when(mockList.get(0)).thenReturn(100);
+        when(mockList.get(1)).thenReturn(100);
+        when(mockList.get(2)).thenReturn(100);
+        when(mockList.get(3)).thenReturn(100);
+
+        cocktailSorter.sort(mockList, 0, false);
+        verify(mockList, times(1)).size();
+        verify(mockList, times(9)).get(any(Integer.class));
+    }
+
+    @Test
+    void testAscendingJSON() throws JSONException {
+        mockJSONList    = mock(List.class);
+        mockJSONObject  = mock(JSONObject.class);
+
+        when(mockJSONList.size()).thenReturn(4);
+        when(mockJSONList.get(any(Integer.class))).thenReturn(new JSONObject());
+
+        CocktailSorter cocktailSorter = new CocktailSorter();
+        assertThrows(JSONException.class, () ->
+                cocktailSorter.sort(
+                        mockJSONList,
+                        0,
+                        true,
+                        new JSONComparator(new ArrayList<>(Collections.singletonList("id")))
+                ));
+
+        // -----------------------------------
+
+        mockJSONList    = mock(List.class);
+        mockJSONObject  = mock(JSONObject.class);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", 3);
+
+        when(mockJSONList.size()).thenReturn(4);
+        when(mockJSONList.get(any(Integer.class))).thenReturn(jsonObject);
+
+        cocktailSorter.sort(
+                mockJSONList,
+                0,
+                true,
+                new JSONComparator(new ArrayList<>(Collections.singletonList("id")))
+        );
+
+        verify(mockJSONList, times(1)).size();
+        verify(mockJSONList, times(6)).get(any(Integer.class));
+    }
+
+    @Test
+    void testDescendingJSON() throws JSONException {
+        mockJSONList    = mock(List.class);
+        mockJSONObject  = mock(JSONObject.class);
+
+        when(mockJSONList.size()).thenReturn(4);
+        when(mockJSONList.get(any(Integer.class))).thenReturn(new JSONObject());
+
+        CocktailSorter cocktailSorter = new CocktailSorter();
+        assertThrows(JSONException.class, () ->
+                cocktailSorter.sort(
+                        mockJSONList,
+                        0,
+                        false,
+                        new JSONComparator(new ArrayList<>(Collections.singletonList("id")))
+                ));
+
+        // -----------------------------------
+
+        mockJSONList    = mock(List.class);
+        mockJSONObject  = mock(JSONObject.class);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", 3);
+
+        when(mockJSONList.size()).thenReturn(4);
+        when(mockJSONList.get(any(Integer.class))).thenReturn(jsonObject);
+
+        cocktailSorter.sort(
+                mockJSONList,
+                0,
+                false,
+                new JSONComparator(new ArrayList<>(Collections.singletonList("id")))
+        );
+
+        verify(mockJSONList, times(1)).size();
+        verify(mockJSONList, times(6)).get(any(Integer.class));
+    }
+}
---
+++ src/test/java/pl/put/poznan/sortingmadness/InsertionSorterTest.java
@@ -0,0 +1,92 @@
+package pl.put.poznan.sortingmadness;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.junit.Test;
+import pl.put.poznan.sortingmadness.logic.InsertionSorter;
+import pl.put.poznan.sortingmadness.logic.JSONComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+
+public class InsertionSorterTest {
+    private List<JSONObject> converter(JSONArray list) throws JSONException {
+        List<JSONObject> jsonObjectList = Mockito.mock(ArrayList.class);
+        for (int i = list.length() - 1; i >= 0; i--) {
+            jsonObjectList.add(list.getJSONObject(i));
+        }
+        return jsonObjectList;
+    }
+
+
+    @Test
+    public void testSortAscendingObj() throws JSONException {
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        JSONArray arr = Mockito.mock(JSONArray.class);
+        JSONObject data1 = Mockito.mock(JSONObject.class);
+        data1.put("name","Ola");
+        data1.put("id","1");
+        JSONObject data2 = Mockito.mock(JSONObject.class);
+        data2.put("name","Ala");
+        data2.put("id","2");
+        JSONObject data3 = Mockito.mock(JSONObject.class);
+        data3.put("name","Ela");
+        data3.put("id","3");
+        arr.put(data1);
+        arr.put(data2);
+        arr.put(data3);
+        List<JSONObject> mockListObj = converter(arr);
+        mockInsertionSorter.sort(mockListObj, 100, true,new JSONComparator(new ArrayList<>(Collections.singleton("id"))));
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyList(), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class), ArgumentMatchers.isA(JSONComparator.class));
+    }
+    @Test
+    public void testSortDescendingObj() throws JSONException {
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        JSONArray arr = Mockito.mock(JSONArray.class);
+        JSONObject data1 = Mockito.mock(JSONObject.class);
+        data1.put("name","Ola");
+        data1.put("id","1");
+        JSONObject data2 = Mockito.mock(JSONObject.class);
+        data2.put("name","Ala");
+        data2.put("id","2");
+        JSONObject data3 = Mockito.mock(JSONObject.class);
+        data3.put("name","Ela");
+        data3.put("id","3");
+        arr.put(data1);
+        arr.put(data2);
+        arr.put(data3);
+        List<JSONObject> mockListObj = converter(arr);
+        mockInsertionSorter.sort(mockListObj, 100, false,new JSONComparator(new ArrayList<>(Collections.singleton("id"))));
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyList(), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class), ArgumentMatchers.isA(JSONComparator.class));
+
+    }
+    @Test
+    public void testSortAscending(){
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        List<Comparable> mockList = Mockito.mock(ArrayList.class);
+        when(mockList.size()).thenReturn(3);
+        when(mockList.get(0)).thenReturn(1);
+        when(mockList.get(1)).thenReturn(2);
+        when(mockList.get(2)).thenReturn(3);
+        mockInsertionSorter.sort(mockList,100,true);
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyListOf(Comparable.class), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class));
+    }
+    @Test
+    public void testSortDescending(){
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        List<Comparable> mockList = Mockito.mock(ArrayList.class);
+        when(mockList.size()).thenReturn(3);
+        when(mockList.get(0)).thenReturn(1);
+        when(mockList.get(1)).thenReturn(2);
+        when(mockList.get(2)).thenReturn(3);
+        mockInsertionSorter.sort(mockList,100,false);
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyListOf(Comparable.class), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class));
+    }
+}
