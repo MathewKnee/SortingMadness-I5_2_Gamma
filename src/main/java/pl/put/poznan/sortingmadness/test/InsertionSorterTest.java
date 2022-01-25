6030e2e7c987cfda88c536d8967e045c1f1e393e
wiktorkoropczuk <wiktor.koropczuk@student.put.poznan.pl>
2022-01-25 02:55:45 +0100

    Adding test request

--- src/main/java/pl/put/poznan/sortingmadness/test/InsertionSorterTest.java
+++ src/main/java/pl/put/poznan/sortingmadness/test/InsertionSorterTest.java
@@ -3,7 +3,9 @@ package pl.put.poznan.sortingmadness.test;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.junit.Test;
 import pl.put.poznan.sortingmadness.logic.InsertionSorter;
 import pl.put.poznan.sortingmadness.logic.JSONComparator;
 
@@ -11,12 +13,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.*;
 
-
-class InsertionSorterTest {
+public class InsertionSorterTest {
     private List<JSONObject> converter(JSONArray list) throws JSONException {
-        List<JSONObject> jsonObjectList = mock(ArrayList.class);
+        List<JSONObject> jsonObjectList = Mockito.mock(ArrayList.class);
         for (int i = list.length() - 1; i >= 0; i--) {
             jsonObjectList.add(list.getJSONObject(i));
         }
@@ -26,15 +26,15 @@ class InsertionSorterTest {
 
     @Test
     public void testSortAscendingObj() throws JSONException {
-        InsertionSorter mockInsertionSorter = mock(InsertionSorter.class);
-        JSONArray arr = mock(JSONArray.class);
-        JSONObject data1 = mock(JSONObject.class);
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        JSONArray arr = Mockito.mock(JSONArray.class);
+        JSONObject data1 = Mockito.mock(JSONObject.class);
         data1.put("name","Ola");
         data1.put("id","1");
-        JSONObject data2 = mock(JSONObject.class);
+        JSONObject data2 = Mockito.mock(JSONObject.class);
         data2.put("name","Ala");
         data2.put("id","2");
-        JSONObject data3 = mock(JSONObject.class);
+        JSONObject data3 = Mockito.mock(JSONObject.class);
         data3.put("name","Ela");
         data3.put("id","3");
         arr.put(data1);
@@ -42,19 +42,19 @@ class InsertionSorterTest {
         arr.put(data3);
         List<JSONObject> mockListObj = converter(arr);
         mockInsertionSorter.sort(mockListObj, 100, true,new JSONComparator(new ArrayList<>(Collections.singleton("id"))));
-        verify(mockInsertionSorter, times(1)).sort(anyList(),isA(Integer.class),isA(Boolean.class),isA(JSONComparator.class));
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyList(), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class), ArgumentMatchers.isA(JSONComparator.class));
     }
     @Test
     public void testSortDescendingObj() throws JSONException {
-        InsertionSorter mockInsertionSorter = mock(InsertionSorter.class);
-        JSONArray arr = mock(JSONArray.class);
-        JSONObject data1 = mock(JSONObject.class);
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        JSONArray arr = Mockito.mock(JSONArray.class);
+        JSONObject data1 = Mockito.mock(JSONObject.class);
         data1.put("name","Ola");
         data1.put("id","1");
-        JSONObject data2 = mock(JSONObject.class);
+        JSONObject data2 = Mockito.mock(JSONObject.class);
         data2.put("name","Ala");
         data2.put("id","2");
-        JSONObject data3 = mock(JSONObject.class);
+        JSONObject data3 = Mockito.mock(JSONObject.class);
         data3.put("name","Ela");
         data3.put("id","3");
         arr.put(data1);
@@ -62,27 +62,27 @@ class InsertionSorterTest {
         arr.put(data3);
         List<JSONObject> mockListObj = converter(arr);
         mockInsertionSorter.sort(mockListObj, 100, false,new JSONComparator(new ArrayList<>(Collections.singleton("id"))));
-        verify(mockInsertionSorter, times(1)).sort(anyList(),isA(Integer.class),isA(Boolean.class),isA(JSONComparator.class));
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyList(), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class), ArgumentMatchers.isA(JSONComparator.class));
 
     }
     @Test
     public void testSortAscending(){
-        InsertionSorter mockInsertionSorter = mock(InsertionSorter.class);
-        List<Comparable> mockList = mock(ArrayList.class);
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        List<Comparable> mockList = Mockito.mock(ArrayList.class);
         mockList.add(1);
         mockList.add(2);
         mockList.add(3);
         mockInsertionSorter.sort(mockList,100,true);
-        verify(mockInsertionSorter, times(1)).sort(anyListOf(Comparable.class),isA(Integer.class),isA(Boolean.class));
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyListOf(Comparable.class), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class));
     }
     @Test
     public void testSortDescending(){
-        InsertionSorter mockInsertionSorter = mock(InsertionSorter.class);
-        List<Comparable> mockList = mock(ArrayList.class);
+        InsertionSorter mockInsertionSorter = Mockito.mock(InsertionSorter.class);
+        List<Comparable> mockList = Mockito.mock(ArrayList.class);
         mockList.add(1);
         mockList.add(2);
         mockList.add(3);
         mockInsertionSorter.sort(mockList,100,false);
-        verify(mockInsertionSorter, times(1)).sort(anyListOf(Comparable.class),isA(Integer.class),isA(Boolean.class));
+        Mockito.verify(mockInsertionSorter, Mockito.times(1)).sort(ArgumentMatchers.anyListOf(Comparable.class), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class));
     }
 }
--- src/main/java/pl/put/poznan/sortingmadness/test/QuickSorterTest.java
+++ src/main/java/pl/put/poznan/sortingmadness/test/QuickSorterTest.java
@@ -3,7 +3,9 @@ package pl.put.poznan.sortingmadness.test;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import pl.put.poznan.sortingmadness.logic.JSONComparator;
 import pl.put.poznan.sortingmadness.logic.QuickSorter;
 
@@ -11,15 +13,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyListOf;
-import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
-
-class QuickSorterTest {
+public class QuickSorterTest {
     private List<JSONObject> converter(JSONArray list) throws JSONException {
-        List<JSONObject> jsonObjectList = mock(ArrayList.class);
+        List<JSONObject> jsonObjectList = Mockito.mock(ArrayList.class);
         for (int i = list.length() - 1; i >= 0; i--) {
             jsonObjectList.add(list.getJSONObject(i));
         }
@@ -29,15 +25,15 @@ class QuickSorterTest {
 
     @Test
     public void testSortAscendingObj() throws JSONException {
-        QuickSorter mockQuickSorter = mock(QuickSorter.class);
-        JSONArray arr = mock(JSONArray.class);
-        JSONObject data1 = mock(JSONObject.class);
+        QuickSorter mockQuickSorter = Mockito.mock(QuickSorter.class);
+        JSONArray arr = Mockito.mock(JSONArray.class);
+        JSONObject data1 = Mockito.mock(JSONObject.class);
         data1.put("name","Ola");
         data1.put("id","1");
-        JSONObject data2 = mock(JSONObject.class);
+        JSONObject data2 = Mockito.mock(JSONObject.class);
         data2.put("name","Ala");
         data2.put("id","2");
-        JSONObject data3 = mock(JSONObject.class);
+        JSONObject data3 = Mockito.mock(JSONObject.class);
         data3.put("name","Ela");
         data3.put("id","3");
         arr.put(data1);
@@ -45,19 +41,19 @@ class QuickSorterTest {
         arr.put(data3);
         List<JSONObject> mockListObj = converter(arr);
         mockQuickSorter.sort(mockListObj, 100, true,new JSONComparator(new ArrayList<>(Collections.singleton("id"))));
-        verify(mockQuickSorter, times(1)).sort(anyList(),isA(Integer.class),isA(Boolean.class),isA(JSONComparator.class));
+        Mockito.verify(mockQuickSorter, Mockito.times(1)).sort(ArgumentMatchers.anyList(), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class), ArgumentMatchers.isA(JSONComparator.class));
     }
     @Test
     public void testSortDescendingObj() throws JSONException {
-        QuickSorter mockQuickSorter = mock(QuickSorter.class);
-        JSONArray arr = mock(JSONArray.class);
-        JSONObject data1 = mock(JSONObject.class);
+        QuickSorter mockQuickSorter = Mockito.mock(QuickSorter.class);
+        JSONArray arr = Mockito.mock(JSONArray.class);
+        JSONObject data1 = Mockito.mock(JSONObject.class);
         data1.put("name","Ola");
         data1.put("id","1");
-        JSONObject data2 = mock(JSONObject.class);
+        JSONObject data2 = Mockito.mock(JSONObject.class);
         data2.put("name","Ala");
         data2.put("id","2");
-        JSONObject data3 = mock(JSONObject.class);
+        JSONObject data3 = Mockito.mock(JSONObject.class);
         data3.put("name","Ela");
         data3.put("id","3");
         arr.put(data1);
@@ -65,27 +61,27 @@ class QuickSorterTest {
         arr.put(data3);
         List<JSONObject> mockListObj = converter(arr);
         mockQuickSorter.sort(mockListObj, 100, false,new JSONComparator(new ArrayList<>(Collections.singleton("id"))));
-        verify(mockQuickSorter, times(1)).sort(anyList(),isA(Integer.class),isA(Boolean.class),isA(JSONComparator.class));
+        Mockito.verify(mockQuickSorter, Mockito.times(1)).sort(ArgumentMatchers.anyList(), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class), ArgumentMatchers.isA(JSONComparator.class));
 
     }
     @Test
     public void testSortAscending(){
-        QuickSorter mockQuickSorter = mock(QuickSorter.class);
-        List<Comparable> mockList = mock(ArrayList.class);
+        QuickSorter mockQuickSorter = Mockito.mock(QuickSorter.class);
+        List<Comparable> mockList = Mockito.mock(ArrayList.class);
         mockList.add(1);
         mockList.add(2);
         mockList.add(3);
         mockQuickSorter.sort(mockList,100,true);
-        verify(mockQuickSorter, times(1)).sort(anyListOf(Comparable.class),isA(Integer.class),isA(Boolean.class));
+        Mockito.verify(mockQuickSorter, Mockito.times(1)).sort(ArgumentMatchers.anyListOf(Comparable.class), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class));
     }
     @Test
     public void testSortDescending(){
-        QuickSorter mockQuickSorter = mock(QuickSorter.class);
-        List<Comparable> mockList = mock(ArrayList.class);
+        QuickSorter mockQuickSorter = Mockito.mock(QuickSorter.class);
+        List<Comparable> mockList = Mockito.mock(ArrayList.class);
         mockList.add(1);
         mockList.add(2);
         mockList.add(3);
         mockQuickSorter.sort(mockList,100,false);
-        verify(mockQuickSorter, times(1)).sort(anyListOf(Comparable.class),isA(Integer.class),isA(Boolean.class));
+        Mockito.verify(mockQuickSorter, Mockito.times(1)).sort(ArgumentMatchers.anyListOf(Comparable.class), ArgumentMatchers.isA(Integer.class), ArgumentMatchers.isA(Boolean.class));
     }
 }
